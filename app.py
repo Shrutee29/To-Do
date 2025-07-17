a8ab8042f1aa6eb8c40bf67162e33a7614c7daf7
Shrutee29 <shruteegonnade2905@gmail,com>
2025-07-17 13:16:30 +0530

    Fix:use dynamic port for render deployment

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def update(sno):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port=int(os.environ.get('PORT',5000))    
+    app.run(debug=False,host='0.0.0.0',port=port)
